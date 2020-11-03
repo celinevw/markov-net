dd54d6b1be90ad50439c2a57a93afba6d263b3be
celinevw <c.j.vanwassenhove@student.tudelft.nl>
2020-11-03 11:41:11 +0100

    Comment code for simulations

--- main.cpp
+++ main.cpp
@@ -7,21 +7,21 @@
 
 int main(int argc, char ** arg) {
 	IO myIO;
-	ParameterObj pars = myIO.read(argc, arg);
+	ParameterObj myparameters;
 
-	NetworkArray network(pars);		// Set up the network
-	const int timesteps = 5000;
+	NetworkArray network(myparameters);		// Set up the network
 	const int num_sims = 10;
 
 	UniformDistribution unif(0,1);
-	auto *myarr_ptr = new std::array<float,num_sims*(2*timesteps+1)>;
+	auto *myarr_ptr = new std::array<float,num_sims>;
 	for (int i=0; i<num_sims; i++){
 		myarr_ptr->at(i) = unif.getRandomNumber(); // CCC Neat use of the .at method, could probably be done even more neatly using a std::foreach but this is fine as is
 	}
 
-	std::vector<ModelInstance*> sims; // create vector of pointers to model objects
-	for (float & it : *myarr_ptr){
-		sims.push_back(new ModelInstance(network, pars, it));
+	ModelInstance myModel(network, myparameters);
+/*	std::vector<ModelInstance*> sims; // create vector of pointers to model objects
+	for (int i=0; i<num_sims; i++){
+		sims.push_back(new ModelInstance(network, myparameters));
 	}
 
 #pragma omp parallel
@@ -33,9 +33,12 @@ int main(int argc, char ** arg) {
 	}
 
 	std::string filepath = "modelOut.tsv";
+	std::ofstream out;
+	out.open(filepath);
+	out << "output" << std::endl;
 	for (ModelInstance* model: sims){
 		myIO.write(*model, filepath);
-	}
+	}*/
 
 	return 0;
 }
