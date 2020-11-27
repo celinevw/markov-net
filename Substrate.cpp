74cccf3ad878e215788905ecda6fb8cf132a76e1
celinevw <c.j.vanwassenhove@student.tudelft.nl>
2020-11-27 14:24:41 +0100

    Use XoShiro pRNG in Susbtrate

--- Substrate.cpp
+++ Substrate.cpp
@@ -4,47 +4,54 @@
 
 #include "Substrate.h"
 
-void Substrate::assign(NetworkArray net, ParameterObj par, bool allow_loading) {
+void Substrate::assign(NetworkArray net, ParameterObj par, bool allow_loading, std::uint64_t seed) {
 	network = net;
 	parameters = par;
 	mult_loading = allow_loading;
 	currenttime = 0.0;
-	complexes.emplace_back(network, parameters);
 	nick1 = -1.0;
 	nick2 = -1.0;
+
+	gen = XoshiroCpp::Xoshiro128PlusPlus (seed);
+	dist = std::uniform_real_distribution<> (0,1);
+	complexes.emplace_back(network, parameters, gen);
 }
 
-Substrate::Substrate(NetworkArray net, ParameterObj par, bool allow_loading){
-	assign(net, par, allow_loading);
+Substrate::Substrate(NetworkArray net, ParameterObj par, bool allow_loading, std::uint64_t seed){
+	assign(net, par, seed, allow_loading);
 }
 
 Substrate::Substrate() {
 	NetworkArray mynet;
 	ParameterObj myparameters;
-	assign(mynet, myparameters, false);
+	std::uint64_t sd {1000};
+	assign(mynet, myparameters, sd, false);
 }
 
 void Substrate::main() {
 	if(!mult_loading){
-		std::vector<float> *numbers_ptr {}; // ToDo: get random numbers
-		complexes.at(0).main(numbers_ptr);
+		complexes.at(0).main();
 		nick1 = complexes.at(0).nick1;
 		nick2 = complexes.at(0).nick2;
 		return;
 	}
 
-	// ToDo: decide when to allow complexes binding
-	//binding moment
-	if (true) {
-		// Adding a complex
-		complexes.emplace_back(network, parameters, currenttime);
+	float x;
+	float dt = complexes.at(0).dt_react;
+	float bindingchance = network.transitions.at(1).at(7);
+	currenttime = dt;
+	while(currenttime < complexes.at(0).totaltime) {
+		x = dist(gen);
+		//binding moment
+		if (x < bindingchance) {
+			complexes.emplace_back(network, parameters, gen, currenttime);
+		}
 	}
 
-	std::vector<float> *numbers_ptr {}; // ToDo: get random numbers
 	std::array<std::vector<float>, 2> nicks;
 	std::array<float,2> nicks_complex {-1.0, -1.0};
 	for(ModelInstance protein : complexes){
-		nicks_complex = protein.main(numbers_ptr);
+		nicks_complex = protein.main();
 		if (nicks_complex.at(0) != -1) {
 			nicks.at(0).push_back(nicks_complex.at(0));
 		}
--- Substrate.h
+++ Substrate.h
@@ -14,15 +14,17 @@ class Substrate{
 private:
 	NetworkArray network;
 	ParameterObj parameters;
+	XoshiroCpp::Xoshiro128PlusPlus gen;
+	std::uniform_real_distribution<> dist;
 public:
-	Substrate(NetworkArray net, ParameterObj par, bool allow_loading);
+	Substrate(NetworkArray net, ParameterObj par, bool allow_loading, std::uint64_t seed);
 	Substrate();
-	void assign(NetworkArray net, ParameterObj par, bool allow_loading);
+	void assign(NetworkArray net, ParameterObj par, bool allow_loading, std::uint64_t seed);
 	std::vector<ModelInstance> complexes;
-	float currenttime;
-	bool mult_loading;
-	float nick1;
-	float nick2;
+	float currenttime{};
+	bool mult_loading{};
+	float nick1{};
+	float nick2{};
 	void main();
 };
 
