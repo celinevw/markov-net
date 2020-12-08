d72de9566cb045c62be9a702e81e2144dc0aa309
celinevw <c.j.vanwassenhove@student.tudelft.nl>
2020-12-08 12:55:30 +0100

    Set initial position to mismatch if trapping experiment, random otherwise

--- Substrate.cpp
+++ Substrate.cpp
@@ -66,15 +66,17 @@ void Substrate::bindComplex(float bindingchance) {
 }
 
 void Substrate::main() {
-	complexes.emplace_back(network, parameters, gen, 0, currenttime, int_dist(gen));
-	positions.push_back(complexes.at(0).getPosition());
-
 	if (!mult_loading) {
+		complexes.emplace_back(network, parameters, gen, 0, currenttime, network.mismatchsite);
+		positions.push_back(complexes.at(0).getPosition());
 		complexes.at(0).main(positions);
 		nick1 = complexes.at(0).nick1;
 		nick2 = complexes.at(0).nick2;
 		return;
 	}
+
+	complexes.emplace_back(network, parameters, gen, 0, currenttime, dist(gen));
+	positions.push_back(complexes.at(0).getPosition());
 
 	float dt_react = complexes.at(0).dt_react;
 	float dt_diff = complexes.at(0).dt_diff;
